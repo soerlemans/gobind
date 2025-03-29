3de0d5362e8fd76f6e94af5b1c794de33e3fa2f0
soerlemans <sebasoerlemans+git@gmail.com>
2025-03-30 00:56:16 +0100

    Finishing up on the C/C++ for now.

--- src/cpp/gobind/golang_module_factory.cpp
+++ src/cpp/gobind/golang_module_factory.cpp
@@ -10,7 +10,21 @@ auto GolangModuleFactory::create_module(const char* t_name) -> Error
 }
 
 auto GolangModuleFactory::compile_module() -> void
-{}
+{
+  // Create function pointer table.
+  const auto size{m_fn_list.size()};
+  auto& fn_table{m_module->m_fn_table};
+
+  golang_function_table_create(&fn_table, size);
+
+  // FIXME: Figure out a safe way to use std::move() here.
+  std::size_t index{0};
+  for(auto& fn : m_fn_list) {
+    fn_table->m_entries[index] = fn;
+
+    index++;
+  }
+}
 
 auto GolangModuleFactory::get_module() -> GolangModule*
 {
