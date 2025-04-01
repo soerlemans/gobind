24dcb915fbe7527534dc9e736141dd0a915fae9b
soerlemans <sebasoerlemans+git@gmail.com>
2025-04-01 19:04:16 +0200

    Did some refactoring.

--- src/go/cgobind/cgobind.go
+++ src/go/cgobind/cgobind.go
@@ -1,6 +1,5 @@
 /*
- * Functions that will interact with the C side of the API.
- * This will be directly.
+ * Bindings for interacting with the generated C dynamic library.
  *
  */
 package cgobind
@@ -33,7 +32,7 @@ import (
 )
 
 // Functions:
-func dlopen(t_path string) (unsafe.Pointer, error) {
+func DlOpen(t_path string) (unsafe.Pointer, error) {
 	handle := C.dlopen(C.CString(t_path), C.RTLD_LAZY_MODE)
 	if handle == nil {
 		err_msg := C.dlerror()
@@ -45,10 +44,10 @@ func dlopen(t_path string) (unsafe.Pointer, error) {
 	return handle, nil
 }
 
-func dlsym(t_handle unsafe.Pointer, t_symbol_name string) (unsafe.Pointer, error) {
+func DlSym(t_handle unsafe.Pointer, t_symbol_name string) (unsafe.Pointer, error) {
 	// Load a symbol (function) from the library
 	symbol := C.dlsym(t_handle, C.CString(t_symbol_name))
-	util.Logf("dlsym(%s): %p", t_symbol_name, symbol)
+	util.Logf("DlSym(%s): %p", t_symbol_name, symbol)
 	if symbol == nil {
 		err_msg := C.dlerror()
 		err := errors.New(C.GoString(err_msg))
@@ -59,14 +58,15 @@ func dlsym(t_handle unsafe.Pointer, t_symbol_name string) (unsafe.Pointer, error
 	return symbol, nil
 }
 
-func dlclose(t_handle unsafe.Pointer) {
+func DlClose(t_handle unsafe.Pointer) {
 	C.dlclose(t_handle)
 }
 
-func gobindRegisteredModules(t_handle unsafe.Pointer) ([]string, error) {
+// Return the names of registered modules.
+func RegisteredModules(t_handle unsafe.Pointer) ([]string, error) {
 	var modules []string
 
-	sym, err := dlsym(t_handle, "gobind_registered_modules")
+	sym, err := DlSym(t_handle, "gobind_registered_modules")
 	if err != nil {
 		return modules, err
 	}
@@ -75,7 +75,7 @@ func gobindRegisteredModules(t_handle unsafe.Pointer) ([]string, error) {
 	gobindModules := (*C.GobindModules)(unsafe.Pointer(voidPtr))
 
 	size := int(gobindModules.m_size)
-	util.Logf("Module count: %d\n", size)
+	util.Logf("Module count: %d", size)
 
 	cStrs := unsafe.Slice(gobindModules.m_modules, size)
 
@@ -83,7 +83,7 @@ func gobindRegisteredModules(t_handle unsafe.Pointer) ([]string, error) {
 	for i, cStr := range cStrs {
 		modules[i] = C.GoString(cStr)
 
-		util.Logf("Modules[%d]: %s\n", i, modules[i])
+		util.Logf("Modules[%d]: %s", i, modules[i])
 	}
 
 	return modules, err
--- src/go/main.go
+++ src/go/main.go
@@ -4,34 +4,13 @@
  */
 package main
 
-/*
-#cgo LDFLAGS: -ldl
-// C Includes:
-#include <dlfcn.h>
-#include <stdio.h>
-
-#include "/home/hackerman/Projects/Git/Personal/gobind/src/cpp/gobind/gobind.h"
-
-// RTLD_LAZY is a macro need to export by assigning it.
-const int RTLD_LAZY_MODE = RTLD_LAZY;
-
-//
-typedef void* (*void_fn_ptr)();
-
-void* call_func(void* t_func) {
-    return ((void_fn_ptr)t_func)();
-}
-*/
-import "C"
-
 // Regular Imports:
 import (
-	"errors"
 	"fmt"
 	"github.com/alexflint/go-arg"
-	"os"
-	"unsafe"
+	"github.com/soerlemans/gobind/cgobind"
 	"github.com/soerlemans/gobind/util"
+	"os"
 	// "text/template"
 )
 
@@ -79,62 +58,6 @@ func initArgs() error {
 	return nil
 }
 
-func dlopen(t_path string) (unsafe.Pointer, error) {
-	handle := C.dlopen(C.CString(t_path), C.RTLD_LAZY_MODE)
-	if handle == nil {
-		err_msg := C.dlerror()
-		err := errors.New(C.GoString(err_msg))
-
-		return handle, err
-	}
-
-	return handle, nil
-}
-
-func dlsym(t_handle unsafe.Pointer, t_symbol_name string) (unsafe.Pointer, error) {
-	// Load a symbol (function) from the library
-	symbol := C.dlsym(t_handle, C.CString(t_symbol_name))
-	util.Logf("dlsym(%s): %p", t_symbol_name, symbol)
-	if symbol == nil {
-		err_msg := C.dlerror()
-		err := errors.New(C.GoString(err_msg))
-
-		return symbol, err
-	}
-
-	return symbol, nil
-}
-
-func dlclose(t_handle unsafe.Pointer) {
-	C.dlclose(t_handle)
-}
-
-func gobindRegisteredModules(t_handle unsafe.Pointer) ([]string, error) {
-	var modules []string
-
-	sym, err := dlsym(t_handle, "gobind_registered_modules")
-	if err != nil {
-		return modules, err
-	}
-
-	voidPtr := C.call_func(sym)
-	gobindModules := (*C.GobindModules)(unsafe.Pointer(voidPtr))
-
-	size := int(gobindModules.m_size)
-	util.Logf("Module count: %d", size)
-
-	cStrs := unsafe.Slice(gobindModules.m_modules, size)
-
-	modules = make([]string, size)
-	for i, cStr := range cStrs {
-		modules[i] = C.GoString(cStr)
-
-		util.Logf("Modules[%d]: %s", i, modules[i])
-	}
-
-	return modules, err
-}
-
 func walkModules(t_registeredModules []string) {
 	for _, name := range t_registeredModules {
 		gobindInitFunctionName := fmt.Sprintf(GOBIND_INIT_FMT, name)
@@ -149,13 +72,13 @@ func main() {
 	}
 
 	// Load the shared library
-	handle, err := dlopen(args.InputFile)
+	handle, err := cgobind.DlOpen(args.InputFile)
 	if err != nil {
 		util.Fail(err)
 	}
-	defer dlclose(handle)
+	defer cgobind.DlClose(handle)
 
-	registeredModules, err := gobindRegisteredModules(handle)
+	registeredModules, err := cgobind.RegisteredModules(handle)
 	if err != nil {
 		util.Fail(err)
 	}
