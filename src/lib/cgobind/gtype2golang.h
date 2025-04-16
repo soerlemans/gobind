516b53c96ff4a2d9119f8f200023d85261f70b9f
Sebastiaan Oerlemans <35037988+soerlemans@users.noreply.github.com>
2025-04-16 11:46:37 +0200

    Added gtype conversions for cgo.

--- src/lib/cgobind/gtype2golang.h
+++ src/lib/cgobind/gtype2golang.h
@@ -9,6 +9,10 @@
 #include <gobind/gobind.h>
 
 // Functions:
+//! Convert a @ref GType to a Golang native type.
 const char* gtype2golang(GType t_gtype);
+
+//! Convert a @ref GType to a Golang cgo type.
+const char* gtype2cgotype(GType t_gtype);
 
 #endif // GTYP2GO_H
